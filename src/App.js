c5036e251b5119b513bd830a14e6cb89cc27f2c6
David Marom <david@yna.co.il>
2021-08-31 10:11:19 +0300

    css contact

--- src/App.js
+++ src/App.js
@@ -18,17 +18,10 @@ function _App() {
 
 	return (
 		<div>
-			<div><TopBar /></div>
+			{/* <div><TopBar /></div> */}
 			<div className="outter-container">
 				<div className="inner-container">
-					<Switch>
-						<Route exact component={Home} path={"/"} />
-						<Route exact component={Cat} path={"/category"} />
-						<Route exact component={Contact} path={"/contact"} />
-						<Route exact component={ProductPage} path={"/product"} />
-						<Route exact component={CartPage} path={"/cart"} />
-						<Route exact component={Auth} path={"/auth"} />
-					</Switch>
+					<Contact />
 				</div>
 			</div>
 		</div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,40 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setPageName, loadDB } from '../store/actions/userActions'
-// import { fdbService } from '../services/fdbService'
 
 const _Contact = () => {
 	const dispatch = useDispatch()
-	useEffect(() => { dispatch(setPageName("Contact")) });
 
 	useEffect(() => { dispatch(loadDB()) }, [dispatch]);
 
 
 	return (
-		<div className="cat-page">
-			<h1>Contact Us</h1>
-			<h3>David Marom: 058-7077017</h3>
+		<div className="contact-container">
+			<div className="contact-left">
+				<h1>
+					The Future of Work in the now: Why you should Become Remote-ready
+				</h1>
+				<h2>
+					Infographic
+				</h2>
+
+				<p>
+					The results are in, and the verdict? Remote is here to stay. Thanks to a global pandemic companies have had to reevaluate the power of distributed workforces and we’ve put together all the reasons why going remote is the right move to make. In this infographics, you’ll see:
+					<ul>
+
+						<li>How remote work broadens the talent pool</li>
+						<li>The productivity results behind distributed teams</li>
+						<li>An increase in diversity as a result of remote recruitment</li>
+						<li>Money saved on operational costs and salary negotiations</li>
+					</ul>
+
+					What better time to refresh your strategy than on the brink of a whole new world? Dig into this list of recruiting methodologies and adjust your sails for the future!
+				</p>
+
+
+
+			</div>
+			<div className="contact-right">right</div>
 		</div>
 	)
 }
